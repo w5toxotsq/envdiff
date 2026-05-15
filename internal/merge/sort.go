d575b249a107026b1711d0512c88aa9878ce5b15
Walt Stokes <w5toxotsq@users.noreply.github.com>
2026-05-15 18:33:37 +0800

    add sortedKeys helper to extract and sort map keys

--- internal/merge/sort.go
+++ internal/merge/sort.go
@@ -2,6 +2,17 @@ package merge
 
 import "sort"
 
+// sortStrings sorts a slice of strings in ascending order in-place.
 func sortStrings(s []string) {
 	sort.Strings(s)
+}
+
+// sortedKeys returns a sorted copy of the keys from the given map.
+func sortedKeys(m map[string]string) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
